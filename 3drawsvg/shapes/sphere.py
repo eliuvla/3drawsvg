9646d33179c304910886f78039c543f4dd5ba4a9
Eliú Vladimir <eliu_vfa@hotmail.com>
2025-10-04 01:55:40 -0600

    creating the first files

---
+++ 3drawsvg/shapes/sphere.py
@@ -0,0 +1,11 @@
+
+
+class Sphere():
+    def __init__(self):
+        self.center = (0,0,0)
+        self.color_stroke = (0,0,0)
+        self.color_fill = (255,255,255)
+        self.radius = 1
+
+    def draw(self):
+        pass
